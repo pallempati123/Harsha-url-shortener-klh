fa3f433c3f7a475093df69453212b2972cdfe42a
PALLEMPATI SRI HARSHAVARDHAN <2210080048@klh.edu.in>
2025-09-19 09:18:17 +0530

    Initial commit

--- frontend/src/Components/UrlResponse.jsx
+++ frontend/src/Components/UrlResponse.jsx
@@ -1,13 +1,29 @@
-import { Text } from '@mantine/core';
-import Service from '../utils/http'
+import { Text, TextInput } from '@mantine/core';
+import Service from '../utils/http';
+import { QRCodeSVG } from 'qrcode.react';
+import { IconCopy } from '@tabler/icons-react';
+import { useClipboard } from '@mantine/hooks';
+
 const obj = new Service();
 
 
 export default function UrlResponse(props) {
-   const surl = obj.getBaseURL() + '/api/s/' + props?.response?.shortCode;
-   return (
-       <div>
-           <Text color="blue"> {surl} </Text>
-       </div>
-   )
+    
+    const clipboard = useClipboard();
+    const surl = obj.getBaseURL() + '/api/s/' + props?.response?.shortCode;
+    return (
+        <div>
+            <Text color="blue"> {surl} </Text>
+            <TextInput value={surl} rightSection={<IconCopy
+                onClick={() => {
+                    console.log("clicked");
+                    clipboard.copy(surl)
+                }}
+            />}
+            />
+
+            <QRCodeSVG imageSettings={{ excavate: true, width: 50, height: 50 }}size={200} value={surl} />
+
+        </div>
+    )
 }
